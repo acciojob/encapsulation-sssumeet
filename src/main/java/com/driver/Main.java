a7a810c3106f81d5a40f132c2d42b46bba81aca7
sssumeet <sumitmankar05@gmail.com>
2022-10-14 20:11:48 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Sumeet");
+        String name = obj.getName();
+
+        System.out.println(name);
+    }
   
 }
